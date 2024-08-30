939d663b385c66ef29881fa0ad7ec63a0dd16ea3
OM2055 <omkar.dba@gmail.com>
2024-08-30 15:33:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,56 @@
 terraform {
-    cloud {
-      organization  = "TCS_BG"
-      workspaces {
-        name   = "terraform-cloud-org"
-      }
+  cloud {
+    organization = "TCS_BG"
+    workspaces {
+      name = "terraform-cloud-org"
     }
+  }
 }
 
+# Declare the TFE provider
 provider "tfe" {
-    hostname = "app.terraform.io"
-    token    =  var.tfe_token
+  hostname = "app.terraform.io"
+  token    = var.tfe_token
 }
 
+# Declare the variable for the TFE token
+variable "tfe_token" {
+  description = "The API token for Terraform Cloud."
+  type        = string
+  sensitive   = true
+}
+
+# Variable for organization name (if needed in other parts of your code)
 variable "organization_name" {
-    description = "Existing TFC Organization"
-    type        = string  
+  description = "Existing TFC Organization"
+  type        = string
+  default     = "TCS_BG"
 }
 
+# Create a Terraform Cloud organization (Optional if it already exists)
 resource "tfe_organization" "test-organization" {
   name  = "TCS_BG"
   email = "omkar.singh1@tcs.com"
 }
 
+# Create an OAuth client for VCS integration
 resource "tfe_oauth_client" "test" {
-  organization     = tfe_organization.test-organization
+  organization     = tfe_organization.test-organization.name
   api_url          = "https://api.github.com"
   http_url         = "https://github.com"
-  oauth_token      = "oauth_token_id"
+  oauth_token      = "your-oauth-token"
   service_provider = "github"
 }
 
+# Create a workspace with VCS integration
 resource "tfe_workspace" "parent" {
-  name                 = "parent-ws"
-  organization         = tfe_organization.test-organization
-  queue_all_runs       = false
+  name         = "parent-ws"
+  organization = tfe_organization.test-organization.name
+  queue_all_runs = false
+
   vcs_repo {
-    branch             = "main"
-    identifier         = "my-org-name/vcs-repository"
-    oauth_token_id     = tfe_oauth_client.test.oauth_token_id
+    branch         = "main"
+    identifier     = "my-org-name/vcs-repository"
+    oauth_token_id = tfe_oauth_client.test.oauth_token_id
   }
 }
